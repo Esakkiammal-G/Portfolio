ed6f56054530c02915a2ae3cc6ac2e32d508a1fc
Esakkiammal G <esakkiammalg.ug22.cs@francisxavier.ac.in>
2025-07-30 20:15:21 +0530

    Fix Profile Image Display

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -25,7 +25,7 @@ const Profile = () => {
           </div>
           <div className="profile-image-container">
             <img
-              src="profile.jpg" 
+              src="Profile.jpg" 
               alt="Esakkiammal G"
               className="profile-image"
             />
